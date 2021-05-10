f346612db46d7b84d605eb5c07e6c51950aaad6b
ZhangYunchenY <55646223+ZhangYunchenY@users.noreply.github.com>
2021-05-10 09:05:05 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/OSExperiment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSExperiment.dir/main.c.o"
+  "OSExperiment"
+  "OSExperiment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSExperiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
